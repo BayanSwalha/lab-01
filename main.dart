a34bcb70ea78cbc0659e96e5814eb102c9745339
BayanSwalha <bayanswalha114@gmail.com>
2022-08-25 17:44:13 +0300

    this is my second upload

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
 void main() {
   String myName = 'Bayan';
+  Map bayanInformation = {
+    "first_name": "Bayan",
+    "last_name": "omar",
+    "age": 23,
+    "eduction info": {
+      "University name": "university of jordan",
+      "faculty": "IT",
+      "Graduation date": 2021
+    }
+  };
 }
